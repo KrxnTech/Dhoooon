30fee562a0342dbb5287bf8b4c634d929c90b6c0
KRISHNA-ai-stack <sharmakrishna1142@gmail.com>
2026-01-28 19:39:52 +0530

    Added 3 song's with some extra info like name , artist , image , id etc.. all in one object array

---
+++ src/MusicData/AllMusic.js
@@ -0,0 +1,29 @@
+// Here we will create an object array which is going to store all the Music
+
+
+
+const AllMusic = [
+    {
+        id: 1,
+        img: "https://m.media-amazon.com/images/M/MV5BYzAzNmZlMDgtZjA5MS00Zjg4LThkNGYtZGFjMjZmNmMwYjdjXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
+        title: "Is There Someone Else 💙",
+        artist: "The Weeknd",
+        src: "/assets/The_Weeknd_-_Is_There_Someone_Else_Audio_128KBPS (1).mp4"
+    },
+    {
+        id: 2,
+        img: "https://www.billboard.com/wp-content/uploads/2020/04/the-weeknd-press-photo-2020-billboard-jgk-1548-1586968737.jpg",
+        title: "Blinding Lights ✨",
+        artist: "The Weeknd",
+        src: "/assets/The_Weeknd_-_After_Hours_Audio_LOW.mp4"
+    },
+    {
+        id: 3,
+        img: "https://i.scdn.co/image/ab67616d0000b2732c0448b550da2511854404d9",
+        title: "I Think They Call This Love 💝",
+        artist: "Yung Kai",
+        src: "/assets/I_Think_They_Call_This_Love_Cover_48kbps.mp4"
+    }
+]
+
+export default AllMusic
