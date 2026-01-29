31acdd7141494596deca0356f4be8a4830caf737
KRISHNA-ai-stack <sharmakrishna1142@gmail.com>
2026-01-29 16:18:43 +0530

    Added Harley in Hawaii song

--- src/MusicData/AllMusic.js
+++ src/MusicData/AllMusic.js
@@ -4,25 +4,32 @@
 
 const AllMusic = [
     {
-        id: 1,
+        id: "0",
         img: "https://m.media-amazon.com/images/M/MV5BYzAzNmZlMDgtZjA5MS00Zjg4LThkNGYtZGFjMjZmNmMwYjdjXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
-        title: "Is There Someone Else 💙",
+        title: "Is There Someone Else ",
         artist: "The Weeknd",
         src: "/assets/The_Weeknd_-_Is_There_Someone_Else_Audio_128KBPS (1).mp4"
     },
     {
-        id: 2,
+        id: "1",
         img: "https://www.billboard.com/wp-content/uploads/2020/04/the-weeknd-press-photo-2020-billboard-jgk-1548-1586968737.jpg",
-        title: "Blinding Lights ✨",
+        title: "Blinding Lights ",
         artist: "The Weeknd",
         src: "/assets/The_Weeknd_-_After_Hours_Audio_LOW.mp4"
     },
     {
-        id: 3,
+        id: "2",
         img: "https://i.scdn.co/image/ab67616d0000b2732c0448b550da2511854404d9",
-        title: "I Think They Call This Love 💝",
+        title: "I Think They Call This Love ",
         artist: "Yung Kai",
         src: "/assets/I_Think_They_Call_This_Love_Cover_48kbps.mp4"
+    },
+    {
+        id: "3",
+        img: "https://www.rollingstone.com/wp-content/uploads/2019/10/KatyPerry.jpg",
+        title: "Harleys in Hawaii",
+        artist: "Katty Perry",
+        src: "/assets/Katy_Perry_-_Harleys_In_Hawaii_Lyrics_You_and_I_Ridin_Harleys_in_Hawaii-i-i_48kbps.mp4"
     }
 ]
 
