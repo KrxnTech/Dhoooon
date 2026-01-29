bb71a34fb4c91717120fd1a98bd76f0e2ccf46bf
KRISHNA-ai-stack <sharmakrishna1142@gmail.com>
2026-01-29 16:17:57 +0530

    Added Router's for Navigation and Params method for Music Selection based on ID

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
-import Home from './components/Home'
-
+import AboutDhoon from './components/AboutDhoon';
+import WelcomePage from './components/WelcomPage';
+import SongList from './components/SongList';
+import Home from "./components/Home";
 function App() {
 
   return (
     <>
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<WelcomePage />} />
+          <Route path="/AboutDhoon" element={<AboutDhoon />} />
+          <Route path="/SongList" element={<SongList />} />
+          <Route path="/Home/:id" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
