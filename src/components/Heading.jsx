c95c59a33c06b0e60a8d2ea8bc080efbba0d031f
KRISHNA-ai-stack <sharmakrishna1142@gmail.com>
2026-01-29 06:33:58 +0530

    Added a new para element for about dhoon

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -2,7 +2,8 @@ import "./Heading.css"
 export default function Heading() {
     return (
         <div>
-            <h1 className="Dhoon">Dhoon 🎵</h1>
+            <h1 className="Dhoon">Dhoon </h1>
+            <p className="About-Dhoon">Your personal music player</p>
         </div>
     )
 }
