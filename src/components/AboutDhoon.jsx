a90d7b4d24bdfcc1272779a66b5958ddd11e1169
KRISHNA-ai-stack <sharmakrishna1142@gmail.com>
2026-01-29 16:22:02 +0530

    Created a Simple About Dhoon Page and added a next and back button for redirecting

---
+++ src/components/AboutDhoon.jsx
@@ -0,0 +1,15 @@
+import { useNavigate } from "react-router-dom"
+import "./AboutDhoon.css"
+export default function AboutDhoon() {
+    const Navigate = useNavigate()
+    return (
+        <div className="AboutDhoonDiv">
+            <h3 className="AboutDhoonTitle">About Dhoon 💗</h3>
+            <p className="AboutDhoonText">It is a personal music player made by KrxnTech</p>
+            <p>Here you can ENJOY music without any Adds and without any cost</p>
+            <button className="Buttons" onClick={() => Navigate("/")}>Back</button>
+            <button className="Buttons" onClick={() => Navigate("/SongList")}>Next</button>
+            <img className="Image" src="public\MusicData\11-117708_anime-little-girl-png-transparent-png-removebg-preview.png" alt="" />
+        </div>
+    )
+} 
