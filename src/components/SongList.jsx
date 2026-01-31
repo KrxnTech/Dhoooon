6e6e33dd889c2c068e4db2932b57953a96b29e09
KRISHNA-ai-stack <sharmakrishna1142@gmail.com>
2026-01-31 17:20:44 +0530

    Code Change and Remove and Id path Navigation Approach and Used the state object concepts to store the song and passing the song in LikedSong.jsx

--- src/components/SongList.jsx
+++ src/components/SongList.jsx
@@ -3,14 +3,46 @@ import AllMusic from "../MusicData/AllMusic";
 import "./SongList.css"
 export default function SongList() {
     const navigate = useNavigate()
+    const HandleAddToFav = (music) => {
+        const Stored = localStorage.getItem("LikedSongs")
+        const LikedSongs = Stored ? JSON.parse(Stored) : []
+
+        const IsAlreadyLiked = LikedSongs.some(song => song.id === music.id)
+
+        if (!IsAlreadyLiked) {
+            const Update = [...LikedSongs, music]
+            localStorage.setItem('LikedSongs', JSON.stringify(Update))
+        } else {
+            alert(`${music.title} is Already in Fav`)
+        }
+
+
+    }
     return (
         <div className="SongListDiv">
-            <p className="MusicListTitle">Song List</p>
+            <p className="MusicListTitle">Song List <button onClick={() => navigate("/LikedSongs")} className="FavSongsButton">Fav Songs</button></p>
+            <div className="SongListHeader">
+                <p className="SongNameP">SongName</p>
+                <p className="AddFavP">Add Fav</p>
+            </div>
+            <br />
             <ul className="ULBASE">
                 {
                     AllMusic.map((Music) => (
-                        <div onClick={() => navigate(`/Home/${Music.id}`)} className="SingleSongListDiv">
-                            <li className="List-Song-title">{Music.title}</li> <p className="PlayIcon">▶</p>
+                        <div key={Music.id} className="SingleSongListDiv">
+                            <img
+                                className="Image"
+                                src={Music.img}
+                                alt="" />
+                            <li
+                                onClick={() => navigate(`/Home/${Music.id}`)}
+                                className="List-Song-title">
+                                {Music.title}
+                            </li>
+                            <i
+                                onClick={() => HandleAddToFav(Music)}
+                                className="HeartIcon">
+                                💗</i>
                         </div>
                     ))
                 }
